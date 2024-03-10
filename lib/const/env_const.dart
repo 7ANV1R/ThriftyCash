9b414ee66abac30dabb0132a90be140f749362ec
7ANV1R <tanvir.officemail@gmail.com>
2024-03-11 00:38:49 +0600

    ✔ learning supabase api

--- lib/const/env_const.dart
+++ lib/const/env_const.dart
@@ -1,4 +1,5 @@
 class EnvConst {
   static const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   static const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+  static const supabaseAPIKey = String.fromEnvironment('API_KEY');
 }
