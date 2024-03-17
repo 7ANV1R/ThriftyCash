539122ee969e039224eef12f2ab2dd12f8b8c23c
7ANV1R <tanvir.officemail@gmail.com>
2024-03-18 02:53:26 +0600

    🏴‍☠️ removed local db file for now

--- lib/data/isolate/db_isolate.dart
+++ lib/data/isolate/db_isolate.dart
@@ -1,37 +1,36 @@
-import 'dart:isolate';
+// import 'dart:isolate';
 
-import 'package:synchronized/synchronized.dart';
+// import 'package:synchronized/synchronized.dart';
 
-import '../../common/ui/logger.dart';
-import '../local_db/trx_data_db.dart';
-import '../services/local_db_services.dart';
+// import '../../common/ui/logger.dart';
+// import '../services/local_db_services.dart';
 
-final mainThredBulkInsertReceivePort = ReceivePort();
+// final mainThredBulkInsertReceivePort = ReceivePort();
 
-final lock = Lock();
+// final lock = Lock();
 
-Future<void> bulkInsertIsolate(List<Map<String, dynamic>> data) async {
-  try {
-    LoggerManager.green('Isolate bulkInsertIsolate');
-    final isolate = await Isolate.spawn(_isolate, [mainThredBulkInsertReceivePort.sendPort, data]);
+// Future<void> bulkInsertIsolate(List<Map<String, dynamic>> data) async {
+//   try {
+//     LoggerManager.green('Isolate bulkInsertIsolate');
+//     final isolate = await Isolate.spawn(_isolate, [mainThredBulkInsertReceivePort.sendPort, data]);
 
-    isolate.addOnExitListener(mainThredBulkInsertReceivePort.sendPort, response: 'Exit');
-  } catch (e) {
-    LoggerManager.red('Isolate Error: $e');
-  }
-}
+//     isolate.addOnExitListener(mainThredBulkInsertReceivePort.sendPort, response: 'Exit');
+//   } catch (e) {
+//     LoggerManager.red('Isolate Error: $e');
+//   }
+// }
 
-Future<void> _isolate(List v) async {
-  try {
-    lock.synchronized(() async {
-      await DatabaseServices().database;
-      final List<Map<String, dynamic>> data = v[1];
-      LoggerManager.green('Isolate data: ${data.length}');
+// Future<void> _isolate(List v) async {
+//   try {
+//     lock.synchronized(() async {
+//       await DatabaseServices().database;
+//       final List<Map<String, dynamic>> data = v[1];
+//       LoggerManager.green('Isolate data: ${data.length}');
 
-      final trxDB = TrxDataDB();
-      await trxDB.batchInsertData(data);
-    });
-  } catch (e) {
-    LoggerManager.red('Error: $e');
-  }
-}
+//       final trxDB = TrxDataDB();
+//       await trxDB.batchInsertData(data);
+//     });
+//   } catch (e) {
+//     LoggerManager.red('Error: $e');
+//   }
+// }
